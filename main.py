8926efe75398bebadc02dd91f085463b02bc1f86
Sankarshan T <mahasankarshant@gmail.com>
2025-08-04 05:09:12 +0530

    changed code

--- main.py
+++ main.py
@@ -17,19 +17,23 @@ spaceship = Spaceship(SCREEN_WIDTH, SCREEN_HEIGHT)
 spaceship_group = pygame.sprite.GroupSingle()
 spaceship_group.add(spaceship)
 
+
 while True:
     #check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-            
+
     #Update:
     spaceship_group.update()
+    
 
     #Drawing
     screen.fill(GREY)
     spaceship_group.draw(screen)
+    spaceship_group.sprite.lasers_group.draw(screen)
+  
 
     pygame.display.update()
     clock.tick(60)
